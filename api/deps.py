2efad0c76991644c6a71945ef3dc5a52db13576c
Igor Nekludov <igor.nekludovv@gmail.com>
2025-07-01 19:07:14 +0300

    rename deps to session in db, add dependencies in api/

---
+++ api/deps.py
@@ -0,0 +1,32 @@
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from fastapi.security import OAuth2PasswordBearer
+from tasket.db.session import get_db_session
+from tasket.db.models.user import User
+from tasket.api.security import decode_token
+import jwt
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
+
+credentials_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Could not validate credentials",
+    headers={"WWW-Authenticate": "Bearer"},
+)
+
+async def get_current_user(token: str = Depends(oauth2_scheme), session: AsyncSession = Depends(get_db_session)) -> User:
+    try:
+        payload = decode_token(token)
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except jwt.PyJWTError:
+        raise credentials_exception
+
+    result = await session.execute(select(User).where(User.username == username))
+    user = result.scalars().first()
+    if user is None:
+        raise credentials_exception
+    return user
