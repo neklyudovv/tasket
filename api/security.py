8f35728c95fd8c50a34c972bc819031a60809ec2
Igor Nekludov <igor.nekludovv@gmail.com>
2026-01-09 02:29:43 +0300

    dont use load_dotenv, pydantic automatically does

--- api/security.py
+++ api/security.py
@@ -1,15 +1,15 @@
 from datetime import datetime, timedelta, UTC
-from config import ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY, ALGORITHM
+from config import settings
 import jwt
 
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.now(UTC) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(UTC) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
 
 
 def decode_token(token: str) -> dict:
-    return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    return jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
--- config.py
+++ config.py
@@ -1,27 +1,18 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import Field
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", case_sensitive=False)
 
-    db_host: str = Field()
-    db_port: int = Field()
-    db_name: str = Field()
-    db_user: str = Field()
-    db_pass: str = Field()
+    db_host: str
+    db_port: int
+    db_name: str
+    db_user: str
+    db_pass: str
 
-    SECRET_KEY: str = Field()
-    ALGORITHM: str = Field()
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = Field()
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
 
 settings = Settings()
