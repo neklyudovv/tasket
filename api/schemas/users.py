fa2984c6a6cf5184b00ea446cd8c40648ecf648b
Igor Nekludov <igor.nekludovv@gmail.com>
2026-01-09 11:19:04 +0300

    set min and max length for username, password

--- api/schemas/users.py
+++ api/schemas/users.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 from datetime import datetime
 
 
 class UserModel(BaseModel):
-    username: str
-    password: str
+    username: str = Field(min_length=3, max_length=20)
+    password: str = Field(min_length=8)
 
 
 class UserRead(BaseModel):
